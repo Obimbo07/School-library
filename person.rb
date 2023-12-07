a253d639b89169790a1c5adf8c126f71bd2a747b
Obimbo07 <obimboausts@gmail.com>
2023-12-07 15:40:22 +0300

    bug-fix

--- person.rb
+++ person.rb
@@ -1,36 +1,3 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError, 'Subclasses must implement the correct_name method'
-  end
-end
-
-class NameableDecorator < Nameable
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < NameableDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < NameableDecorator
-  MAX_LENGTH = 10
-
-  def correct_name
-    trimmed_name = @nameable.correct_name[0, MAX_LENGTH]
-    trimmed_name.sub!(/\s\w*\z/, '')
-    trimmed_name
-  end
-end
-
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
