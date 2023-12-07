692ae8274939ecf1c155a35d1a74fb0eee4b09f9
Obimbo07 <obimboausts@gmail.com>
2023-12-07 15:15:54 +0300

    decorator

--- person.rb
+++ person.rb
@@ -1,9 +1,43 @@
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Subclasses must implement the correct_name method'
+  end
+end
+
+class NameableDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < NameableDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < NameableDecorator
+  MAX_LENGTH = 10
+
+  def correct_name
+    trimmed_name = @nameable.correct_name[0, MAX_LENGTH]
+    trimmed_name.sub!(/\s\w*\z/, '')
+    trimmed_name
+  end
+end
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(name: 'Unknown', age: 0, parent_permission: true)
-    @id = generate_id
+  def initialize(id, name = 'Unknown', age: nil, parent_permission: true)
+    super()
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -13,13 +47,13 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
-    @age >= 18
-  end
-
-  def generate_id
-    rand(1000..9999)
+    @age.to_i >= 18
   end
 end
